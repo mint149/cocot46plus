66441524fab052d8ddc1e59ea170116180ab68a7
Mint <skylove.exe@gmail.com>
2023-04-08 10:45:26 +0900

    最新版対応

--- config.h
+++ config.h
@@ -20,11 +20,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0x1727
-#define PRODUCT_ID   0x0003
-#define DEVICE_VER   0x0001
-#define MANUFACTURER aki27
-#define PRODUCT      cocot46plus
+// #define VENDOR_ID    0x1727
+// #define PRODUCT_ID   0x0003
+// #define DEVICE_VER   0x0001
+// #define MANUFACTURER aki27
+// #define PRODUCT      cocot46plus
 
 /* VIA */
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7
